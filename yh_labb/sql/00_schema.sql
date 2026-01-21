d4d6d85bbf5766fdc8191735205d8563d6230936
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-01-21 20:50:59 +0100

    fix: i changed the tables in the roles file so that everything goes hand in hand with my physical model

--- yh_labb/sql/00_schema.sql
+++ yh_labb/sql/00_schema.sql
@@ -1,3 +1,4 @@
 -- Creating schema 'skol_info' and setting it as the default search path for the database 'yrkesco'
 CREATE SCHEMA IF NOT EXISTS skol_info;
+-- is set to defualt search path so that the new tables land in skol_info
 ALTER DATABASE yrkesco SET search_path TO skol_info, public; 
--- yh_labb/sql/03_roles.sql
+++ yh_labb/sql/03_roles.sql
@@ -3,7 +3,7 @@
 -- Student inherit from Person
 CREATE TABLE student (
     person_id INT PRIMARY KEY REFERENCES person(person_id) ON DELETE CASCADE,
-    enrollment_date DATE DEFAULT current_date
+    enrollment_date DATE,
     class_id INT
 );
 
@@ -14,4 +14,13 @@ CREATE TABLE employee (
     department VARCHAR(50)
 );
 
+-- Consultant inherits from Person
+CREATE TABLE consultant (
+    person_id INT PRIMARY KEY REFERENCES person(person_id) ON DELETE CASCADE,
+    company VARCHAR(100),
+    contract_end DATE,
+    f_skatt BOOLEAN,
+    org_number VARCHAR(20),
+    hourly_rate NUMERIC(8,2)
+);
 
