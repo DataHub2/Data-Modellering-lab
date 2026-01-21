89783323e666a690ef95bcdcd1e737a492756317
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-01-21 18:49:14 +0100

    feat: added the employee table to the roles file

--- yh_labb/sql/03_roles.sql
+++ yh_labb/sql/03_roles.sql
@@ -2,6 +2,13 @@
 
 -- Student inherit from Person
 CREATE TABLE student (
-    peroson_id INT PRIMARY KEY REFERENCES person(person_id) on DELETE CASCADE,
+    peroson_id INT PRIMARY KEY REFERENCES person(person_id) ON DELETE CASCADE,
     enrollment_date DATE DEFAULT current_date
+);
+
+-- Employee inherits from Person
+CREATE TABLE Employee (
+    person_id INT PRIMARY KEY REFERENCES person(person_id) ON DELETE CASCADE,
+    title VARCHAR(50) NOT NULL, -- This title can be 'Educational leader, Teacher' etc.
+    department VARCHAR(50)
 );
