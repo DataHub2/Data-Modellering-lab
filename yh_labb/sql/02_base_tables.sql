7943a9b826f098a1ec7ab71661df4e945d4bec0e
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-01-21 18:12:52 +0100

    feat: added the location table

--- yh_labb/sql/02_base_tables.sql
+++ yh_labb/sql/02_base_tables.sql
@@ -5,3 +5,9 @@ CREATE TABLE program (
     name VARCHAR(100) NOT NULL,
     description TEXT
 );
+
+CREATE TABLE school (
+    location_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL
+);
