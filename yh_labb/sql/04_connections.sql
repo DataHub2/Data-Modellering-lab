7e4616fbf902f4e7858a13399ba4a90648c81088
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-01-22 15:13:07 +0100

    fix: fixed my class tabkle to inclued start and end date, so i can use scenrio with diffrent class years

--- yh_labb/sql/04_connections.sql
+++ yh_labb/sql/04_connections.sql
@@ -5,6 +5,8 @@
 CREATE TABLE class (
     class_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
+    start_date DATE,
+    end_date DATE,
     program_id INT REFERENCES program(program_id) ON DELETE CASCADE,
     location_id INT REFERENCES location(location_id),
     education_leader_id INT REFERENCES person(person_id) 
