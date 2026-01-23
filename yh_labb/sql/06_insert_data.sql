398cbad678377b33f07fc4af54c61585604137b4
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-01-23 13:47:10 +0100

    feat: inserted the program data

--- yh_labb/sql/06_insert_data.sql
+++ yh_labb/sql/06_insert_data.sql
@@ -5,3 +5,7 @@ INSERT INTO location (name, address) VALUES
 ('Stockholm', 'Sveavägen 10'),
 ('Göteborg', 'Kungsportsavenyen 5');
 
+INSERT INTO program (name, description) VALUES
+('Systemutveckling', 'Advanced backend och strategi'),
+('Data Engineer', 'Big Data, Cloud och AI-lösningar');
+
