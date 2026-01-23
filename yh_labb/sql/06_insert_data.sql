9dcc380562ced6eb4e0c8c227778353f2b4ed25e
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-01-23 14:02:19 +0100

    feat: inserted the course data

--- yh_labb/sql/06_insert_data.sql
+++ yh_labb/sql/06_insert_data.sql
@@ -9,3 +9,11 @@ INSERT INTO program (name, description) VALUES
 ('Systemutveckling', 'Advanced backend och strategi'),
 ('Data Engineer', 'Big Data, Cloud och AI-lösningar');
 
+INSERT INTO course (course_code, name, points, description) VALUES
+('JAV-ADV', 'Avancerad Java & Spring Boot', 50, 'Backend-utveckling med moderna ramverk'),
+('DATA-ENG', 'Big Data & Cloud Architecture', 40, 'Hantering av stora datamängder i AWS/Azure'), 
+('DB-SQL', 'Datalagring & SQL', 25, 'Databasdesign och avancerade querys'); 
+
+
+
+
