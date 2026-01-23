d7790e71039d47f93d4c3253c0f2a01e3d7507e8
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-01-23 13:04:34 +0100

    feat: inserted the location data

---
+++ yh_labb/sql/06_insert_data.sql
@@ -0,0 +1,7 @@
+-- This file is for inserting the data in to the tables
+
+-- This is  the Base Data 
+INSERT INTO location (name, address) VALUES
+('Stockholm', 'Sveavägen 10'),
+('Göteborg', 'Kungsportsavenyen 5');
+
